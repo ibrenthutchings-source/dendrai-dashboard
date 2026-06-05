bba1dfe1639541232cfb2b091aa4160681801034
ibrenthutchings-source <i.brent.hutchings@gmail.com>
2026-06-05 00:23:57 +0000

    modified:   project/package.json 	modified:   project/vite.config.js

--- project/vite.config.js
+++ project/vite.config.js
@@ -1,6 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import AutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    AutoImport({
+      imports: [
+        {
+          react: [
+            ['default', 'React'],
+            'useState',
+            'useEffect',
+            'useRef',
+            'useMemo',
+            'useCallback',
+            'useLayoutEffect',
+            'useContext',
+            'useReducer',
+            'useImperativeHandle',
+            'useDebugValue'
+          ]
+        }
+      ],
+      dts: true,
+      eslintrc: {
+        enabled: false,
+      },
+    }),
+  ],
 })
